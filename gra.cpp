d30982891c0da1899875ecb8e879e8b52620dfe0
Malina1304 <albert.malinowski1304@gmail.com>
2026-06-10 11:33:45 +0200

    poprawki

--- gra.cpp
+++ gra.cpp
@@ -17,27 +17,45 @@ for(int i=0;i<10;i++){
 for(int i=0;i<10;i++){
     t[0][i]='#';
     t[9][i]='#';
-}
-for(int i=0;i<10;i++){
     t[i][0]='#';
     t[i][9]='#';
 }
-do{
-   for(int i=0;i<5;i++){
+while(pkt<5){
+    //poczatkowe resetowanie planszy
+    for(int i=1;i<8;i++){
+        for(int j=1;j<8;j++){
+        t[i][j]=' ';
+        }
+   }
+   t[x][y]='G';
+   t[8][8]='M';
+   //generacja przeszkod
+    for(int i=0;i<20;i++){
         int a = rand()%10;
         int b = rand()%10;
         if(t[a][b]==' '){
             t[a][b]='#';
         }
-   }
+   } //zeby dalo sie wygrac
+    for(int i=1;i<8;i++){
+        if(t[i][8]=='#'){
+            t[i][8]=' ';
+        }
+        if(t[8][i]=='#'){
+            t[8][i]=' ';
+        }
+    }
+    t[1][2]=' ';
+    t[2][1]=' ';
     for(int i=0;i<10;i++){
         for(int j=0;j<10;j++){
             cout<<t[i][j];
     }
         cout<<endl;
-}
-    while(x+y!=16){
+} //poruszanie sie
+    while(x+y<16){
         t[x][y]='G';
+        t[8][8]='M';
         char poz;
         cout<<"Gdzie chcesz pojsc?"<<endl<<"w/s/a/d"<<endl;
         cin>>poz;
@@ -49,19 +67,17 @@ do{
                 cout<<"Przegrales, -1pkt"<<endl;
                 if(pkt!=0){pkt--;}
                 swap(t[x][y],t[1][1]);
-                x=1;
-                y=1;
+                x=1; y=1;
                 for(int i=1;i<8;i++){
                     for(int j=1;j<8;j++){
                         if(t[i][j]=='#'){
                             t[i][j]=' ';
                         }
                     }
-                }
-                break;
+                } break;
             }
         }
-        if(poz=='d'){
+        else if(poz=='d'){
             if(t[x][y+1]!='#'){
             swap(t[x][y],t[x][y+1]);
             y++;}
@@ -69,19 +85,17 @@ do{
                 cout<<"Przegrales, -1pkt"<<endl;
                 if(pkt!=0){ pkt--;}
                 swap(t[x][y],t[1][1]);
-                x=1;
-                y=1;
+                x=1; y=1;
                 for(int i=1;i<8;i++){
                     for(int j=1;j<8;j++){
                         if(t[i][j]=='#'){
                             t[i][j]=' ';
                         }
                     }
-                }
-                break;
+                } break;
             }
         }
-        if(poz=='s'){
+        else if(poz=='s'){
             if(t[x+1][y]!='#'){
             swap(t[x][y],t[x+1][y]);
             x++;}
@@ -89,19 +103,17 @@ do{
                 cout<<"Przegrales, -1pkt"<<endl;
                 if(pkt!=0){ pkt--;}
                 swap(t[x][y],t[1][1]);
-                x=1;
-                y=1;
+                x=1; y=1;
                 for(int i=1;i<8;i++){
                     for(int j=1;j<8;j++){
                         if(t[i][j]=='#'){
                             t[i][j]=' ';
                         }
                     }
-                }
-                break;
+                } break;
             }
         }
-        if(poz=='w'){
+       else if(poz=='w'){
             if(t[x-1][y]!='#'){
             swap(t[x][y],t[x-1][y]);
             x--;}
@@ -109,17 +121,23 @@ do{
                 cout<<"Przegrales, -1pkt"<<endl;
                 if(pkt!=0){pkt--;}
                 swap(t[x][y],t[1][1]);
-                x=1;
-                y=1;
+                x=1; y=1;
                 for(int i=1;i<8;i++){
                     for(int j=1;j<8;j++){
                         if(t[i][j]=='#'){
                             t[i][j]=' ';
                         }
                     }
-                }
-                break;
+                } break;
             }
+        }
+        if(t[8][8]!='M'){
+            t[7][8]=' ';
+            t[8][7]=' ';
+            x=1; y=1;; pkt++;
+            cout<<"Brawo, +1pkt"<<endl;
+            swap(t[8][8],t[1][1]);
+            break;
         }
         for(int i=0;i<10;i++){
             for(int j=0;j<10;j++){
@@ -128,9 +146,7 @@ do{
         cout<<endl;
         }
     }
-    if(x+y==16){
-        pkt++;
-        cout<<"Brawo, +1pkt"<<endl;
     }
-}while(pkt!=5);
-}czm program sie konczy?
+    cout<<"Gratulacje! Wygrales :)"<<endl;
+}
+
